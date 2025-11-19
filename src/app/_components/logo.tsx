7a8dd43194f3e5279616381751cf0d1b5dff6f27
itigoppo <itigoppo+github@gmail.com>
2025-11-19 11:32:51 +0900

    余計なスタイルあった

--- src/app/_components/logo.tsx
+++ src/app/_components/logo.tsx
@@ -4,12 +4,7 @@ import { cn } from "@/lib/utils/common"
 
 export function Logo() {
   return (
-    <div
-      className={cn(
-        "flex items-center space-x-2 px-3 py-4 backdrop-blur-md transition-transform",
-        exo2.className
-      )}
-    >
+    <div className={cn("flex items-center space-x-2 px-3 py-4", exo2.className)}>
       {/* Animated Icon */}
       <IconWandStars className="size-9 animate-pulse text-lime-500" />
 
